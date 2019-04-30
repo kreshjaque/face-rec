c8ed6d820bb0f2b59c2caa8e5f047562beb4abfb
Krishna Kumar <kreshjaque@gmail.com>
2019-04-30 13:00:20 +0530

    added pre commit hooks

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import * as faceApi from 'face-api.js';
-import WebCam from "react-webcam";
+import WebCam from 'react-webcam';
 import './App.css';
 import { orderBy } from 'lodash';
 
@@ -12,29 +12,33 @@ export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      expression: "",
+      expression: '',
       timerId: null
     };
     this.webcam = React.createRef();
   }
 
   analyse = async () => {
-    console.log("called", this.webcam);
-    this.setState({ expression: "" });
+    console.log('called', this.webcam);
+    this.setState({ expression: '' });
     const image = new Image();
     image.src = this.webcam.getScreenshot();
-    const detectionsWithExpressions = await faceApi.detectAllFaces(image).withFaceExpressions();
-    console.log("expressions", detectionsWithExpressions);
+    const detectionsWithExpressions = await faceApi
+      .detectAllFaces(image)
+      .withFaceExpressions();
+    console.log('expressions', detectionsWithExpressions);
     if (detectionsWithExpressions && detectionsWithExpressions.length !== 0) {
-      const expression = orderBy(detectionsWithExpressions[0].expressions, ['probability'], ['desc'])[0]['expression'];
-      console.log("expression", expression);
-      this.setState(
-        {
-          image: image.src,
-          expression
-        }
-      );
-      if (expression !== "happy") {
+      const expression = orderBy(
+        detectionsWithExpressions[0].expressions,
+        ['probability'],
+        ['desc']
+      )[0]['expression'];
+      console.log('expression', expression);
+      this.setState({
+        image: image.src,
+        expression
+      });
+      if (expression !== 'happy') {
         this.setState({ oldImage: image.src });
         this.analyse();
       }
@@ -42,9 +46,9 @@ export default class App extends React.Component {
   };
 
   async componentDidMount() {
-    await faceApi.loadFaceDetectionModel("models");
-    await faceApi.loadFaceExpressionModel("models");
-    await faceApi.loadSsdMobilenetv1Model('models')
+    await faceApi.loadFaceDetectionModel('models');
+    await faceApi.loadFaceExpressionModel('models');
+    await faceApi.loadSsdMobilenetv1Model('models');
   }
   startProcess() {
     setTimeout(() => {
@@ -57,30 +61,40 @@ export default class App extends React.Component {
       <div className="container">
         <img alt="not happy" className="nothappy" src={this.state.image} />
       </div>
-    )
+    );
   }
 
   render() {
     const videoConstraints = {
       width: 350,
       height: 350,
-      facingMode: 'user',
+      facingMode: 'user'
     };
     return (
       <div className="App">
         <div className="App-header">
-          {this.state.expression === "happy" ? this.getSuccess() : 'sad'}
+          {this.state.expression === 'happy' ? this.getSuccess() : 'sad'}
           <WebCam
             audio={false}
             height={350}
             ref={this.setRef}
             screenshotFormat="image/jpeg"
             width={350}
-            onUserMedia={() => { this.startProcess() }}
+            onUserMedia={() => {
+              this.startProcess();
+            }}
             videoConstraints={videoConstraints}
           />
-          <div className="expression">{this.state.expression ? this.state.expression : ""}</div>
-          <button onClick={() => { this.analyse() }} >Detect</button>
+          <div className="expression">
+            {this.state.expression ? this.state.expression : ''}
+          </div>
+          <button
+            onClick={() => {
+              this.analyse();
+            }}
+          >
+            Detect
+          </button>
         </div>
       </div>
     );
