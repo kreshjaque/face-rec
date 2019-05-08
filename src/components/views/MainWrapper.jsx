3b735c561826045fa5c83e8f4a34d6c80ef160aa
Krishna Kumar <kreshjaque@gmail.com>
2019-05-08 15:21:15 +0530

    added comp

---
+++ src/components/views/MainWrapper.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MainWrapper = () => {
+  return (
+    <main>
+      <h1>Alive</h1>
+      <p>Every thing else goes here</p>
+    </main>
+  );
+};
+
+export default MainWrapper;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import MainWrapper from './components/views/MainWrapper';
 
 const App = () => {
-    // eslint-disable-next-line react/jsx-filename-extension
-    return <h1>Alive</h1>;
+  // eslint-disable-next-line react/jsx-filename-extension
+  return <MainWrapper />;
 };
 
 ReactDOM.render(<App />, document.getElementById('root'));
