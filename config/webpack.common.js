947bcdd7cd7b0ef71f61b96b37f94c4ef99a5300
Krishna Kumar <kreshjaque@gmail.com>
2021-01-02 19:58:00 +0530

    added couple of config

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -1,16 +1,33 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
-	module.exports = {
-		entry: { 
-			index: path.resolve(__dirname, "../src", "index.js") 
-		},
-		output: {
-			path: path.resolve(__dirname, "../dist")
-		},
-		plugins: [
-			new HtmlWebpackPlugin({
-				template: path.resolve(__dirname, "../src", "index.html")
-			})
-		]
-	};
+module.exports = {
+  entry: {
+    index: path.resolve(__dirname, '../src', 'index.js'),
+  },
+  output: {
+    path: path.resolve(__dirname, '../dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+    ],
+  },
+  optimization: {
+    splitChunks: { chunks: 'all' },
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, '../src', 'index.html'),
+    }),
+  ],
+};
--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log("Hello webpack!")
+import React, { useState } from 'react';
+import { render } from 'react-dom';
+import './common.css';
+
+function App() {
+  const [state, setState] = useState('CLICK ME');
+
+  return (
+    <button type="button" onClick={() => setState('CLICKED')}>
+      {state}
+    </button>
+  );
+}
+
+render(<App />, document.getElementById('root'));
