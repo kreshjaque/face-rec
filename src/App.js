8b96aa3d8d2c9a9d33d01682c3d8062ae1654f47
Krishna Kumar <kreshjaque@gmail.com>
2021-03-05 14:03:42 +0530

    used maincontainer

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React, { useState } from 'react';
 import { render } from 'react-dom';
 import { Heading } from './common.styles';
+import MainContainer from './components/MainContainer/MainContainer';
 
 const App = () => {
   const [state, setState] = useState('CLICK ME');
 
   return (
-    <>
+    <MainContainer>
       <Heading>Alive</Heading>
       <button type="button" onClick={() => setState('CLICKED')}>
         {state}
       </button>
       ]
-    </>
+    </MainContainer>
   );
-}
+};
 
 render(<App />, document.getElementById('root'));
